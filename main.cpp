c684c00b973186b78bfc06b74437086804930dc9
CappedMonke <schuergersilasdev@gmail.com>
2024-12-19 16:40:36 +0100

    Updated CMakeLists

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include <QPushButton>
-
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    QPushButton button("Hello, Qt6!");
-    button.show();
-    return app.exec();
-}
