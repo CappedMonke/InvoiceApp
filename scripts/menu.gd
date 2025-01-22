19fac9bd9330ae7a8da72566a302ab755c72298e
CappedMonke <schuergersilasdev@gmail.com>
2025-01-22 16:01:59 +0100

    rework

---
+++ scripts/menu.gd
@@ -0,0 +1,29 @@
+extends PanelContainer
+
+@onready var new_window: Window = $Buttons/NewButton/Window
+@onready var open_window: FileDialog = $Buttons/OpenButton/FileDialog
+@onready var shortcuts_window: Window = $Buttons/ShortcutsButton/Window
+@onready var settings_window: Window = $Buttons/SettingsButton/Window
+
+func _on_new_button_pressed() -> void:
+	new_window.show()
+
+
+func _on_open_button_pressed() -> void:
+	open_window.show()
+
+
+func _on_save_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_export_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_shortcuts_button_pressed() -> void:
+	shortcuts_window.show()
+
+
+func _on_settings_button_pressed() -> void:
+	settings_window.show()
---
+++ scripts/window.gd
@@ -0,0 +1,9 @@
+extends Window
+
+
+func _on_close_requested() -> void:
+	hide()
+
+
+func _on_focus_exited() -> void:
+	hide()
